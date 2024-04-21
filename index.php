180e23a6a3570fd844263ca87774d74882bcfa8a
web-devteam <154674696+web-devteam@users.noreply.github.com>
2024-04-21 20:39:12 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+// the message
+$msg = "First line of text\nSecond line of text";
+
+// use wordwrap() if lines are longer than 70 characters
+$msg = wordwrap($msg,70);
+
+// send email
+mail("buluswise@gmail.com","My subject",$msg);
+
+// the message
+$msg = "First line of text\nSecond line of text";
+
+// use wordwrap() if lines are longer than 70 characters
+$msg = wordwrap($msg,70);
+
+// send email
+mail("web.dev.team321@gmail.com","My subject",$msg);
+?>
